cb3fff26e565b2dce23c780721f8ea4c191bf9f0
rajmohan <rajmohan@mindbricks.com>
2010-12-12 09:44:14 +0530

    Fix for stun re-transmission timer value calculation
    Fix for stun re-transmission timer value calculation. The
    re-transmission timer value was wrongly being computed due to which Rc
    number of re-transmissions were being done but the timer value was
    wrong. This is fixed in this commit. Now the overall transmission of
    stun client request ends at 39.5 secs with recommended default values.

--- stun/txn/src/stun_txn_fsm.c
+++ stun/txn/src/stun_txn_fsm.c
@@ -186,8 +186,7 @@ int32_t resend_req (stun_txn_context_t *txn_ctxt, handle h_msg)
         }
         else
         {
-            uint32_t new_rto = 
-                txn_ctxt->instance->rto + (2 * txn_ctxt->last_rto);
+            uint32_t new_rto = 2 * txn_ctxt->last_rto;
             txn_ctxt->h_rto_timer = txn_ctxt->instance->start_timer_cb(
                                                 new_rto, txn_ctxt->rto_params);
             ICE_LOG (LOG_SEV_DEBUG, "RTO timer handle %p\n", txn_ctxt->h_rto_timer);
